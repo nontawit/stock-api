28bd4d95917097e4e54d63ca65b8ce018c6e5394
nontawit <ntwitmn@gmail.com>
2024-10-23 00:38:00 +0700

    Update pool.js

--- src/db/pool.js
+++ src/db/pool.js
@@ -3,9 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  user: 'postgres',
+  host: '54.254.162.138', // ไม่ใช่ 127.0.0.1
+  database: 'stockdb',
+  password: 'Ntw0622433755.',
+  port: 5432,
+});
   
 module.exports = pool;
